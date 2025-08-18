eb1fa36e7c9a254512351ee1dd51601801d230f0
AltoPal0 <161604323+AltoPal0@users.noreply.github.com>
2025-08-18 12:06:29 +0200

    fix on message

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -297,13 +297,14 @@ export const UserProfile = () => {
                     Complete Your Founder Profile
                   </Button>
                 </div>
-              )} else {
-                <div className="mt-0">
+              )} 
+               {supabaseUser.profiler_profile_name && (
+                <div className="mt-3">
                   <p className="text-sm text-green-700 mt-1">
-                    Your application has been approved.  Stay tuned for the platform launch.
+                    Your application has been approved. Complete your founder profile to get the most out of the network.
                   </p>
                 </div>
-              }
+               )}
             </div>
           </div>
         </div>
